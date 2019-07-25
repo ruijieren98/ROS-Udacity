657bb1e530df44642a9166df2a1f33682e9ccced
ruijieren98 <ruijieren98@gmail.com>
2019-07-25 08:55:52 +0000

    add robot to the world

---
+++ L2/Project2/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/ROS-Udacity/L2/Project2/catkin_ws/src
+# Build directory: /home/workspace/ROS-Udacity/L2/Project2/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
---
+++ L2/Project2/catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/workspace/ROS-Udacity/L2/Project2/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
