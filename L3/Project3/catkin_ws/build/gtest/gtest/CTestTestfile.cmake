6990614b09a5189a8c7b5c98a3b9c6c4e2cd4794
ruijieren98 <ruijieren98@gmail.com>
2019-10-04 03:30:38 +0000

    compile

---
+++ L3/Project3/catkin_ws/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/workspace/ROS-Udacity/L3/Project3/catkin_ws/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ L3/Project3/catkin_ws/build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/ROS-Udacity/L3/Project3/catkin_ws/src/my_robot
+# Build directory: /home/workspace/ROS-Udacity/L3/Project3/catkin_ws/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
