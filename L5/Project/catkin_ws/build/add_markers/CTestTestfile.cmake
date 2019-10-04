9d84496d7bc811d5dd910f6c875890331bbbce33
ruijieren98 <ruijieren98@gmail.com>
2019-10-04 05:59:30 +0000

    init_ws

--- L5/Project/catkin_ws/build/add_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/ROS-Udacity/L5/Project/catkin_ws/src/add_markers
-# Build directory: /home/workspace/ROS-Udacity/L5/Project/catkin_ws/build/add_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- L5/Project/catkin_ws/build/pick_objects/CMakeFiles/pick_objects_generate_messages_eus.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pick_objects_generate_messages_eus"
-  "/home/workspace/ROS-Udacity/L5/Project/catkin_ws/devel/share/roseus/ros/pick_objects/manifest.l"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pick_objects_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
