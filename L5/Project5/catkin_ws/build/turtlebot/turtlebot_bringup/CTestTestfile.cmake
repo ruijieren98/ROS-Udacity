eb63cf5d85a94c1505f03159456fa6edb4f82d3f
ruijieren98 <ruijieren98@gmail.com>
2019-10-04 06:27:10 +0000

    compile

---
+++ L5/Project5/catkin_ws/build/turtlebot/turtlebot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/ROS-Udacity/L5/Project5/catkin_ws/src/turtlebot/turtlebot_bringup
+# Build directory: /home/workspace/ROS-Udacity/L5/Project5/catkin_ws/build/turtlebot/turtlebot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
